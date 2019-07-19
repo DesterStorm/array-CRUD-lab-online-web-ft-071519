c8fbb3d6f1a8e7ce59bef0af2ef6afb561b8043f
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-19 02:03:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,23 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["wow", "I", "am", "really", "learning"]
+  array = ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
-   array = ["one", "two", "three", "four"]
-   element = "five"
+   array = ["wow", "I", "am", "really", "learning"]
+   element = "arrays!"
    array << element
 end
 
 def add_element_to_start_of_array(array, element)
-   array = ["one", "two", "three", "four", "five"]
-   element = "six"
+   array = ["I", "am", "really", "learning", "arrays!"]
+   element = "wow"
    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["one", "two", "three", "four", "five", "six"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.pop()
 end
 
